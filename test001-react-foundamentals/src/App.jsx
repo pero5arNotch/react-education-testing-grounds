d24632a67f32f566590f0337e90e82a325e7ccb3
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-26 13:56:27 +0200

    [checkpoint] first component

--- test001-react-foundamentals/src/App.jsx
+++ test001-react-foundamentals/src/App.jsx
@@ -1,37 +1,48 @@
 import logo from './logo.png';
 
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <img src={logo} height={50} style={{ backgroundColor: 'white', padding: 10, borderRadius: 10, margin: 10 }} alt="logo" />
+      </div>
+      <div>
+        <h1>Hello World!</h1>
+        <span>
+          Dobrodošli na današnje predavanje!
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function Cell(props) {
+  const { title, color } = props;
+
+  return (
+    <div style={{ backgroundColor: color, padding: '5%', height: '10em' }}>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+function Content() {
+  return (
+    <div className="content" style={{ color: 'white', textAlign: 'center' }}>
+      <Cell title="A" color="#F3A712" />
+      <Cell title="B" color="#FF0054" />
+      <Cell title="C" color="#3772FF" />
+      <Cell title="D" color="#BAA898" />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="app">
       <div style={{ backgroundColor: '#51D6A9', margin: 10, padding: "10px" }}>
-
-        <div className="header">
-          <div>
-            <img src={logo} height={50} style={{ backgroundColor: 'white', padding: 10, borderRadius: 10, margin: 10 }} alt="logo" />
-          </div>
-          <div>
-            <h1>Hello World!</h1>
-            <span>
-              Dobrodošli na današnje predavanje!
-            </span>
-          </div>
-        </div>
-
-        <div className="content" style={{ color: 'white', textAlign: 'center' }}>
-          <div style={{ backgroundColor: '#F3A712', padding: '5%', height: '10em' }}>
-            <h2>A</h2>
-          </div>
-          <div style={{ backgroundColor: '#FF0054', padding: '5%', height: '10em' }}>
-            <h2>B</h2>
-          </div>
-          <div style={{ backgroundColor: '#3772FF', padding: '5%', height: '10em' }}>
-            <h2>C</h2>
-          </div>
-          <div style={{ backgroundColor: '#BAA898', padding: '5%', height: '10em' }}>
-            <h2>D</h2>
-          </div>
-        </div>
-
+        <Header />
+        <Content />
       </div>
     </div>
   );
