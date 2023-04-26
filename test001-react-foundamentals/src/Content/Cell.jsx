656d53d9505c219a6c0e3aeba03428c79750be4f
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-26 14:11:50 +0200

    [checkpoint] before state

--- test001-react-foundamentals/src/Content/Cell.jsx
+++ test001-react-foundamentals/src/Content/Cell.jsx
@@ -1,9 +1,12 @@
 function Cell(props) {
     const { title, color } = props;
 
+    const counter = 0;
+
     return (
         <div style={{ backgroundColor: color, padding: '5%', height: '10em' }}>
             <h2>{title}</h2>
+            <span>Čelija je kliknuta {counter} puta.</span>
         </div>
     )
 }
