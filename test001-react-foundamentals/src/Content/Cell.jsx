89b39e18b5e28b1d181df3612d6b9fe2218f5c07
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-26 14:16:14 +0200

    [checkpoint] basic counter implemented

--- test001-react-foundamentals/src/Content/Cell.jsx
+++ test001-react-foundamentals/src/Content/Cell.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 function Cell(props) {
     const { title, color } = props;
 
-    const counter = 0;
+    const [counter, setCounter] = useState(0);
 
     return (
-        <div style={{ backgroundColor: color, padding: '5%', height: '10em' }}>
+        <div
+            style={{ backgroundColor: color, padding: '5%', height: '10em' }}
+            onClick={() => setCounter(counter + 1)}
+        >
             <h2>{title}</h2>
             <span>Čelija je kliknuta {counter} puta.</span>
         </div>
