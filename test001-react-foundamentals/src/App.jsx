374e5355b94e394786af6e67b668d2a0a153fc0e
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-27 14:57:02 +0200

    [checkpoint] render list start

--- test001-react-foundamentals/src/App.jsx
+++ test001-react-foundamentals/src/App.jsx
@@ -3,11 +3,15 @@ import Content from './Content';
 
 function App() {
   console.log('UPDATE: App');
+
+  const whiteTextCells = [{ title: 'A', color: '#F3A712' }, { title: 'B', color: '#FF0054' }];
+  const blackTextCells = [{ title: 'C', color: '#3772FF' }, { title: 'D', color: '#BAA898' }];
+
   return (
     <div className="app">
       <div style={{ backgroundColor: '#51D6A9', margin: 10, padding: "10px" }}>
         <Header />
-        <Content />
+        <Content whiteTextCells={whiteTextCells} blackTextCells={blackTextCells} />
       </div>
     </div>
   );
