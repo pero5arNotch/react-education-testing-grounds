7391bf37f2ec3a3e760f847df0e7f77226b3798e
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-27 11:56:02 +0200

    [checkpoint] useCallback

--- test001-react-foundamentals/src/Content/index.jsx
+++ test001-react-foundamentals/src/Content/index.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Cell from './Cell';
 
 function Content() {
   const [totalCount, setTotalCount] = useState(0);
-  const handleClick = () => setTotalCount((oldValue) => oldValue + 1);
+  // const handleClick = () => setTotalCount((oldValue) => oldValue + 1);
+  const handleClick = useCallback(() => setTotalCount((oldValue) => oldValue + 1), []);
 
   console.log('UPDATE: Content');
   return (
     <div className="content" style={{ color: 'white', textAlign: 'center' }}>
-      <Cell title="A" color="#F3A712" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="B" color="#FF0054" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="C" color="#3772FF" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="D" color="#BAA898" totalCount={totalCount} onClick={handleClick} />
+      <Cell title="A" color="#F3A712" totalCount={0} onClick={handleClick} />
+      <Cell title="B" color="#FF0054" totalCount={0} onClick={handleClick} />
+      <Cell title="C" color="#3772FF" totalCount={0} onClick={handleClick} />
+      <Cell title="D" color="#BAA898" totalCount={0} onClick={handleClick} />
     </div>
   );
 }
