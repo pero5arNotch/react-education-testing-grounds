9518551806151142272d8aac737960b399f42b5b
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-27 15:19:15 +0200

    [checkpoint] add element: start

--- test001-react-foundamentals/src/App.jsx
+++ test001-react-foundamentals/src/App.jsx
@@ -1,11 +1,22 @@
 import Header from './Header';
 import Content from './Content';
+import { useState } from 'react';
 
 function App() {
   console.log('UPDATE: App');
 
-  const whiteTextCells = [{ title: 'A', color: '#F3A712' }, { title: 'B', color: '#FF0054' }];
-  const blackTextCells = [{ title: 'C', color: '#3772FF' }, { title: 'D', color: '#BAA898' }, { title: 'Test', color: 'aquamarine' }];
+  const [whiteTextCells, setWhiteTextCells] = useState([{ title: 'A', color: '#F3A712' }, { title: 'B', color: '#FF0054' }]);
+  const [blackTextCells, setBlackTextCells] = useState([{ title: 'C', color: '#3772FF' }, { title: 'D', color: '#BAA898' }, { title: 'Test', color: 'aquamarine' }]);
+
+  const addCell = (newCell) => {
+    const { title, color, textColor } = newCell;
+
+    if (textColor === 'white') {
+      setWhiteTextCells((oldValue) => [...oldValue, { title, color }]);
+    } else if (textColor === 'black') {
+      setBlackTextCells((oldValue) => [...oldValue, { title, color }]);
+    }
+  };
 
   return (
     <div className="app">
