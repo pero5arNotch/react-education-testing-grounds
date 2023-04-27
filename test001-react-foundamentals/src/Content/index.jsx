d24a98e8f8fe135744194edff2e315be56266f28
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-04-27 12:07:39 +0200

    [checkpoint] before useMemo

--- test001-react-foundamentals/src/Content/index.jsx
+++ test001-react-foundamentals/src/Content/index.jsx
@@ -6,13 +6,15 @@ function Content() {
   // const handleClick = () => setTotalCount((oldValue) => oldValue + 1);
   const handleClick = useCallback(() => setTotalCount((oldValue) => oldValue + 1), []);
 
+  const cellStyle = { padding: '5%', height: '10em' };
+
   console.log('UPDATE: Content');
   return (
     <div className="content" style={{ color: 'white', textAlign: 'center' }}>
-      <Cell title="A" color="#F3A712" totalCount={0} onClick={handleClick} />
-      <Cell title="B" color="#FF0054" totalCount={0} onClick={handleClick} />
-      <Cell title="C" color="#3772FF" totalCount={0} onClick={handleClick} />
-      <Cell title="D" color="#BAA898" totalCount={0} onClick={handleClick} />
+      <Cell title="A" color="#F3A712" totalCount={0} onClick={handleClick} style={cellStyle} />
+      <Cell title="B" color="#FF0054" totalCount={0} onClick={handleClick} style={cellStyle} />
+      <Cell title="C" color="#3772FF" totalCount={0} onClick={handleClick} style={cellStyle} />
+      <Cell title="D" color="#BAA898" totalCount={0} onClick={handleClick} style={cellStyle} />
     </div>
   );
 }
