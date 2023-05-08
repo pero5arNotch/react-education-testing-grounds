d7b6e4606f9f3189e03f668304fc205db88482b5
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-05-08 17:33:19 +0200

    add bootstrap

--- test003-react-example/simple-dnd-tool/src/main.tsx
+++ test003-react-example/simple-dnd-tool/src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import root CSS
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
